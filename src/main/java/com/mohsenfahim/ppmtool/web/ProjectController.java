0e079059707b530c84ebb5502b1d9ee259f6f01d
Fahim <fahimm@cubic.com>
2020-01-13 15:44:45 -0800

    created MapValidationErrorService for result checking

--- src/main/java/com/mohsenfahim/ppmtool/web/ProjectController.java
+++ src/main/java/com/mohsenfahim/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.mohsenfahim.ppmtool.web;
 
 import com.mohsenfahim.ppmtool.domain.Project;
+import com.mohsenfahim.ppmtool.services.MapValidationReturnService;
 import com.mohsenfahim.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,20 +25,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationReturnService mapValidationReturnService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject (@Valid @RequestBody Project project, BindingResult result){
 
-        if (result.hasErrors()){
-
-            Map<String, String> errorMap = new HashMap<>();
-
-            for (FieldError error:result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            //return new ResponseEntity<List<FieldError>>( result.getFieldErrors(), HttpStatus.BAD_REQUEST);
-            return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationReturnService.MapValidationReturnService(result);
+        if (errorMap != null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
