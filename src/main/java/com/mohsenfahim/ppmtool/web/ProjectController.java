59612a8d4eb7ad9252f1d16b6a9a062f23f713a0
Fahim <fahimm@cubic.com>
2020-01-13 10:07:43 -0800

    added @PostMapping to project controller

--- src/main/java/com/mohsenfahim/ppmtool/web/ProjectController.java
+++ src/main/java/com/mohsenfahim/ppmtool/web/ProjectController.java
@@ -5,6 +5,7 @@ import com.mohsenfahim.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @PostMapping
     public ResponseEntity<Project> createNewProject (@RequestBody Project project){
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
