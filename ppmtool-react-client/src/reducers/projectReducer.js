0165fa17a0ec496fc04c4df04cacf4158305928e
Fahim <fahimm@cubic.com>
2020-03-05 13:17:44 -0800

    added react client application back into repository correctly

---
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -0,0 +1,34 @@
+import { GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "../actions/types";
+
+const intialState = {
+  projects: [],
+  project: {}
+};
+
+export default function(state = intialState, action) {
+  switch (action.type) {
+    case GET_PROJECTS:
+      return {
+        ...state,
+        projects: action.payload
+      };
+
+    case GET_PROJECT:
+      return {
+        ...state,
+        project: action.payload
+      };
+
+    case DELETE_PROJECT:
+      return {
+        ...state,
+        //projects: action.payload //this line would make a GET call to backend but following filter function removes the deleted project from the list using redux
+        projects: state.projects.filter(
+          project => project.projectIdentifier !== action.payload
+        )
+      };
+
+    default:
+      return state;
+  }
+}
